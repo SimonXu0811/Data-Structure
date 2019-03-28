1c5ebcb8f7a0567a2b25e3791ccd307033ce2b90
SimonXu0811 <48972207+SimonXu0811@users.noreply.github.com>
2019-03-28 17:33:10 +0800

    Update Bubblesort.java

--- Array/Sort/BubbleSort/Bubblesort.java
+++ Array/Sort/BubbleSort/Bubblesort.java
@@ -2,7 +2,7 @@ public void BubbleSort (int[] nums){
    for(int i = 0; i < nums.length - 1; i++){
    //外层循环控制内层循环循环的长度，这边时间占用T（n）
       for(int j = 0; j < nums.length - 1 - i; j++){
-      // 内层循环作为交换的索引
+      // 内层循环作为交换的索引，这里时间占用为T（n/2）
         if(nums[j]>nums[j + 1]){
           swap(nums[j],nums[j + 1]);
         }
@@ -15,3 +15,8 @@ public void swap(int a,int b){
    a = b;
    b = a;
 }
+
+/*
+时间复杂度：O（n^2）
+空间复杂度：O（1）
+*/
